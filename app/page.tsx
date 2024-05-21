f4616b2576eef438ad268c9a5719a53e874fd3cf
r0_xyz <maxhard243@gmail.com>
2024-05-22 01:57:35 +0300

    hotfix: change import

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { MainPage } from './../src/pages/main-page/main-page';
+import MainPage from './../src/pages/main-page/main-page';
 
 export default function Home() {
   return (
